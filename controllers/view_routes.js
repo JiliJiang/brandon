32b3552f5064e2edc2268ed105bacaba2df19609
JiliJiang <jiangjili@gmail.com>
2023-11-10 18:52:36 -0800

    from tutoring session

---
+++ controllers/view_routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const {User, Post} = require('../models');
+const withAuth =  require('../utils/auth')
+
+router.get('/', (req, res) => {res.render('landingpage')});
+
+// router.get('/', async (req, res) => {
+//     const posts = await Post.findAll({
+//         include: [User]
+//     });
+
+//     res.render('landingpage', {
+//         logged_in: req.session.logged_in,
+//         posts: posts.map(c => c.get({ plain: true}))
+//     });
+// });
+
+router.get('/register',  (req, res) => {
+    res.render('registerform' );
+});
+
+router.get('/dashboard', withAuth, (req, res) => {
+    res.render('dashboard' );
+});
+
+router.get('/login', (req, res) => {
+    res.render('loginform');
+});
+
+// router.get('/post',  (req, res) =>{
+//     res.render('postform', {
+//         logged_in: req.session.logged_in,
+//     });
+
+// });
+
+module.exports = router;
